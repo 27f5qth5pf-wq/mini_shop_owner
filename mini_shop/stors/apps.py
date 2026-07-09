905529b6a1557778cc3ff36ba7e6adb4c93eb1db
Shakoor Afridi <shakoorkhan031894@gmail.com>
2026-07-10 00:29:00 +0500

    home chanhe in html

--- mini_shop/stors/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StorsConfig(AppConfig):
-    name = 'stors'
--- minishop/minishop/views.py
+++ minishop/minishop/views.py
@@ -12,17 +12,28 @@ def about (request):
 def analyzes (request):
     djtext = request.GET.get('text','default')
     removepunc = request.GET.get('removepunc','off')
-    upper
+    Upper_case = request.GET.get('Upper_case','off')
     
     # analyzed = djtext
     functuations  = """`.,?!:;'"()[]}-{—…/\\|_@#$%&*+=^~``"""
     analyzed = ""
-    for char in djtext:
-        if char not in functuations:
-            analyzed = analyzed + char
 
-    params = {'purpose': 'remove funchuation ','analyzed_text':analyzed}
-    return render(request,'analyzed.html',params)
+    if removepunc == "on":
+        for char in djtext:
+            if char not in functuations:
+                analyzed = analyzed + char
+
+        params = {'purpose': 'remove funchuation ','analyzed_text':analyzed}
+        return render(request,'analyzed.html',params)
+
+    elif Upper_case == "on":
+        for char in djtext:
+            if char not in functuations:
+                analyzed = analyzed + char.upper
+
+        params = {'purpose': 'remove funchuation ','analyzed_text':analyzed}
+        return render(request,'analyzed.html',params)
+
 
 
 
